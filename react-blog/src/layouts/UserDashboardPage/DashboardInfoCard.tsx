9547aeab97cb8e8450f6b764988c8c724b97e7f4
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2024-01-18 04:50:21 +0100

    minor style fixes

--- react-blog/src/layouts/UserDashboardPage/DashboardInfoCard.tsx
+++ react-blog/src/layouts/UserDashboardPage/DashboardInfoCard.tsx
@@ -1,17 +1,31 @@
 import {DashboardInfoCardParams} from "./DashboardInfoCardParams";
+import React from "react";
 
 export const DashboardInfoCard = (props) => {
     return (
 
-            <div className="card" style={{background: 'transparent', border: 'none'}}>
-                <img src={require("../../Images/PublicImages/coco.jpg")}
-                     className="card-img-top shadow-lg" alt="user-photo"
+        <div className="card" style={{background: 'transparent', border: 'none'}}>
+            {props.user.avatar === '' || props.user.avatar === undefined  ||  props.user.avatar === null ?
+                <img src={require("../../Images/PublicImages/profile-placeholder-image.jpg")}
+                     className="card-img-top" alt="user-photo"
                      style={{borderRadius: '350px', scale: '75%'}}/>
-                <div className="card-body pt-0">
-                    <h6 className="card-title px-5">{props.user.about}</h6>
+                :
+                <img src={props.user.avatar} alt="user-avatar"
+                     className="card-img-top shadow-lg"
+                     style={{borderRadius: '350px', scale: '65%', border: '1px solid black'}}/>
 
-                </div>
-                <DashboardInfoCardParams user={props.user}/>
+            }
+
+            <div className="card-body pt-0">
+                <h6 className="card-title px-5">{props.user.about}</h6>
+
             </div>
+            <DashboardInfoCardParams user={props.user}/>
+        </div>
     );
 }
+
+/*
+<img className="shadow-lg" src={article?.image} alt="article"/>
+:
+<img src={require('../../Images/ArticlesImages/default.png')} alt="article"/>*/
--- react-blog/src/layouts/UserDashboardPage/DashboardInfoCardParams.tsx
+++ react-blog/src/layouts/UserDashboardPage/DashboardInfoCardParams.tsx
@@ -2,7 +2,7 @@ import {DashboardInfoCardParamRow} from "./DashboardInfoCardParamRow";
 
 export const DashboardInfoCardParams = (props) => {
     return (
-        <ul className="list-group list-group-flush px-4">
+        <ul className="list-group list-group-flush">
             <DashboardInfoCardParamRow name='First name' value = {props.user.firstName}/>
             <DashboardInfoCardParamRow name='Last name' value = {props.user.lastName}/>
             <DashboardInfoCardParamRow name='Birth date' value = {props.user.birthDate}/>
